8b3e53fb8d11600febbf27e08db8466f060d51a2
Pritam Sanyal <sanyal.pritam538@gmail.com>
2025-06-23 15:07:29 +0530

    new added

--- Apitesting/src/test/java/apitest/Restapitest.java
+++ Apitesting/src/test/java/apitest/Restapitest.java
@@ -1,7 +1,20 @@
 package apitest;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -11,10 +24,27 @@ public class Restapitest {
 	String token;
 	String phonenumber;
 	String emailaddress;
+	ExtentSparkReporter spark;
+	ExtentReports report;
+	ExtentTest test;
 	@BeforeTest
 	public void startup()
 	{
 		RestAssured.baseURI="https://dev.mantramedic.com";
+		String reportPath = System.getProperty("user.dir") + "/test-output/extentreport.html";
+		spark = new ExtentSparkReporter(reportPath);
+		report=new ExtentReports();
+		report.attachReporter(spark);
+	    spark.config().setDocumentTitle(" Rest Assured Api Automation");
+        spark .config().setTheme(Theme.DARK);
+        spark.config().setReportName("Rest Assured Api Automation");
+        spark.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+		
+	}
+	@BeforeMethod
+	public void createtest(Method method)
+	{
+		test=report.createTest(method.getName());
 	}
 	
 	@Test(priority = 1 )
@@ -45,6 +75,32 @@ public class Restapitest {
 		System.out.println("Extracted email address:- "+ emailaddress);
 		
 		
+	}
+	@AfterMethod
+	public void resultstatus(ITestResult result)throws IOException
+	
+	{
+		if(result.getStatus()==ITestResult.SUCCESS)
+		{
+			test.log(Status.PASS, result.getMethod().getMethodName() + " PASSED");
+			test.log(Status.PASS, result.getThrowable());
+		}
+		else if (result.getStatus()==ITestResult.FAILURE) {
+			test.log(Status.FAIL, result.getMethod().getMethodName() + " FAILED");
+			test.log(Status.FAIL, result.getThrowable());
+			
+		}
+		else
+		{
+			test.log(Status.SKIP, result.getMethod().getMethodName() + " SKIPPED");
+			test.log(Status.SKIP,result.getThrowable());
+		}
+		
+	}
+	@AfterClass
+	public void reportend()
+	{
+		report.flush();
 	}
 	
 	
